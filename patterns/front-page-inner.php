88f37f93f702edc1eb8b429511aec7cdff77c784
Giacomo Secchi <giacomo.secchi@gmail.com>
2024-07-02 21:30:10 +0200

    Remove old banners

---
+++ patterns/front-page-inner.php
@@ -0,0 +1,204 @@
+<?php
+/**
+ * Title: Old covers to remove
+ * Slug: write-white/front-page-inner
+ * Categories: footer
+ * Block Types: core/template-part/footer
+ * Keywords: newsletter, subscribe, button
+ */
+?>
+
+        <!-- wp:column {"verticalAlignment":"center"} -->
+        <div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"style":{"border":{"radius":"100px"},"spacing":{"padding":{"top":"16px","right":"16px","bottom":"16px","left":"16px"}}},"backgroundColor":"white","className":"is-style-inline","layout":{"contentSize":"100px"}} -->
+        <div class="wp-block-group is-style-inline has-white-background-color has-background" style="border-radius:100px;padding-top:16px;padding-right:16px;padding-bottom:16px;padding-left:16px"><!-- wp:image {"align":"center","width":40,"height":40,"sizeSlug":"large"} -->
+        <figure class="wp-block-image aligncenter size-large is-resized"><img src="
+        
+        data:image/svg+xml,%0A%3Csvg width='40' height='40' viewBox='0 0 83 66' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='
+        
+        M28.9992 33.0008C32.2769 33.0011 35.4811 32.0337 38.2066 30.2208C40.9321 28.4079 43.0564 25.831 44.311 22.8159C45.5655 19.8009 45.8939 16.4832 45.2547 13.2823C44.6154 10.0815 43.0372 7.14121 40.7196 4.83343C38.402 2.52565 35.4492 0.953976 32.2345 0.317175C29.0197 -0.319627 25.6876 0.00704428 22.6593 1.25587C19.631 2.5047 17.0427 4.6196 15.2217 7.33311C13.4007 10.0466 12.4287 13.2369 12.4287 16.5004C12.4285 18.6672 12.8569 20.8128 13.6896 22.8147C14.5222 24.8166 15.7428 26.6356 17.2815 28.1679C18.8202 29.7001 20.647 30.9155 22.6576 31.7448C24.6681 32.574 26.823 33.0008 28.9992 33.0008ZM35.563 39.1883H22.4354C19.489 39.1883 16.5715 39.7661 13.8494 40.8888C11.1273 42.0115 8.65395 43.6571 6.57061 45.7316C4.48727 47.806 2.83472 50.2688 1.70733 52.9792C0.579944 55.6896 -0.000208883 58.5945 2.60131e-06 61.5282C-0.000632259 62.1152 0.114944 62.6966 0.340125 63.2391C0.565305 63.7816 0.895672 64.2746 1.31234 64.6899C1.72901 65.1053 2.2238 65.4347 2.76844 65.6595C3.31307 65.8843 3.89687 66 4.48644 66H53.5071C54.0972 66.0008 54.6817 65.8858 55.2271 65.6614C55.7725 65.4369 56.268 65.1076 56.6854 64.6922C57.1027 64.2768 57.4337 63.7835 57.6593 63.2406C57.8848 62.6976 58.0006 62.1157 58 61.5282C57.9996 55.6034 55.6355 49.9214 51.4279 45.7319C47.2202 41.5425 41.5135 39.1887 35.563 39.1883V39.1883Z' fill='%230693E3'/%3E%3Cpath d='M61.7328 41.1958H51.979C58.0478 46.065 61.8511 53.3528 61.8511 61.518C61.8477 63.1025 61.3926 64.6547 60.5368 66H78.7728C81.1107 66 83 64.1407 83 61.7495C82.9852 50.4457 73.5271 41.1958 61.7328 41.1958ZM55.5112 32.9261C57.8565 32.9352 60.1704 32.3985 62.2619 31.3603C64.3533 30.322 66.1622 28.812 67.5393 26.9549C68.9163 25.0979 69.822 22.947 70.1815 20.68C70.5409 18.413 70.3439 16.0948 69.6067 13.9169C68.8695 11.7391 67.6132 9.76401 65.9416 8.15481C64.27 6.54562 62.2311 5.34847 59.9931 4.66223C57.7551 3.97598 55.3824 3.82034 53.0708 4.20813C50.7592 4.59593 48.575 5.51605 46.6987 6.89251C48.2217 9.75138 49.1713 12.9509 49.1713 16.3883C49.1627 20.7997 47.6999 25.0906 45 28.6241C46.3727 29.9874 48.009 31.0698 49.8136 31.8084C51.6182 32.547 53.555 32.9269 55.5112 32.9261V32.9261Z
+        ' fill='%239B51E0'/%3E%3C/svg%3E%0A
+        " alt="WhatsApp English group logo" width="40" height="40"/></figure>
+        <!-- /wp:image --></div>
+        <!-- /wp:group -->
+
+        <!-- wp:paragraph {"textColor":"white","fontSize":"large"} -->
+        <p class="has-white-color has-text-color has-large-font-size">Wanna practice every day?</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:heading {"textColor":"white"} -->
+        <h2 class="has-white-color has-text-color" id="h-join-whatsapp-english-speaking-club"><strong>Join WhatsApp English Speaking Club</strong></h2>
+        <!-- /wp:heading -->
+
+        <!-- wp:html -->
+        <ul class="fa-ul">
+                                                                <li class="has-white-color has-text-color" style="padding: 0.4em;"><span class="fa-li"><i class="fa-duotone fa-check-double has-primary-color has-background fa-beat-fade fa-xl"></i></span>Content and Exercises delivered every day</li>
+                                                        <li class="has-white-color has-text-color" style="padding: 0.4em;"><span class="fa-li"><i class="fa-duotone fa-check-double has-primary-color has-background fa-beat-fade fa-xl"></i></span>Exclusive access to a continuously growing community</li>
+                                                                <li class="has-white-color has-text-color" style="padding: 0.4em;"><span class="fa-li"><i class="fa-duotone fa-check-double has-primary-color has-background fa-beat-fade fa-xl"></i></span>Learn and practice with other students from all around the world</li>
+                                                                <li class="has-white-color has-text-color" style="padding: 0.4em;"><span class="fa-li"><i class="fa-duotone fa-check-double has-primary-color has-background fa-beat-fade fa-xl"></i></span>Get personal feedback and focus on your individual study plan</li>
+                                                                </ul>
+        <!-- /wp:html --></div>
+        <!-- /wp:column --></div>
+        <!-- /wp:columns -->
+
+        <!-- wp:spacer {"height":"50px"} -->
+        <div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
+        <!-- /wp:spacer -->
+
+        <!-- wp:columns {"style":{"spacing":{"blockGap":"1em"}}} -->
+        <div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center"} -->
+        <div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"backgroundColor":"white","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+        <div class="wp-block-group has-white-background-color has-background"><!-- wp:heading {"textAlign":"center","level":4} -->
+        <h4 class="has-text-align-center" id="h-one-month-subscription">One month subscription</h4>
+        <!-- /wp:heading -->
+
+
+        <!-- wp:paragraph {"align":"center"} -->
+        <p class="has-text-align-center">Try for one month</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:paragraph {"align":"center","fontSize":"large"} -->
+        <p class="has-text-align-center has-large-font-size"><strong><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-luminous-vivid-orange-color">20 €</mark></strong><sub>/month</sub></p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:buttons -->
+        <div class="wp-block-buttons"><!-- wp:button {"textColor":"luminous-vivid-orange","align":"center","style":{"border":{"radius":"100px"}},"className":"is-style-outline"} -->
+        <div class="wp-block-button aligncenter is-style-outline"><a class="wp-block-button__link has-luminous-vivid-orange-color has-text-color" href="https://www.easyenglishwithcristina.com/product/whatsapp-speaking-club-subscription/?attribute_billing-period=1+Month" style="border-radius:100px"><strong>Join now</strong></a></div>
+        <!-- /wp:button --></div>
+        <!-- /wp:buttons --></div>
+        <!-- /wp:group --></div>
+        <!-- /wp:column -->
+
+        <!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} -->
+        <div class="wp-block-column is-vertically-aligned-center" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:group {"style":{"border":{"radius":{"topLeft":"8px","topRight":"8px","bottomLeft":"0px","bottomRight":"0px"}},"spacing":{"padding":{"right":"6px","left":"6px","top":"6px","bottom":"6px"},"blockGap":"0px"}},"backgroundColor":"luminous-vivid-orange","textColor":"white","layout":{"type":"flex","orientation":"vertical"}} -->
+        <div class="wp-block-group has-white-color has-luminous-vivid-orange-background-color has-text-color has-background" style="border-top-left-radius:8px;border-top-right-radius:8px;border-bottom-left-radius:0px;border-bottom-right-radius:0px;padding-top:6px;padding-right:6px;padding-bottom:6px;padding-left:6px"><!-- wp:paragraph {"align":"center","fontSize":"small"} -->
+        <p class="has-text-align-center has-small-font-size">★ <strong>The most popular</strong></p>
+        <!-- /wp:paragraph --></div>
+        <!-- /wp:group -->
+
+        <!-- wp:group {"style":{"border":{"style":"solid","width":"1px"},"spacing":{"margin":{"top":"0px"}}},"borderColor":"luminous-vivid-orange","backgroundColor":"white","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+        <div class="wp-block-group has-border-color has-luminous-vivid-orange-border-color has-white-background-color has-background" style="border-style:solid;border-width:1px;margin-top:0px"><!-- wp:heading {"textAlign":"center","level":4} -->
+        <h4 class="has-text-align-center" id="h-3-months-subscription">3 months subscription🔥</h4>
+        <!-- /wp:heading -->
+
+		<!-- wp:paragraph {"align":"center","fontSize":"large"} -->
+		<p class="has-text-align-center has-large-font-size"><mark style="background-color:#f02d3a;color:#ffffff" class="has-inline-color"><strong>10% </strong>OFF!</mark></p>
+		<!-- /wp:paragraph -->
+
+        <!-- wp:paragraph {"align":"center","fontSize":"large"} -->
+        <p class="has-text-align-center has-large-font-size"><strong><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-luminous-vivid-orange-color">54 €</mark></strong><sub>/month</sub></p>
+        <!-- /wp:paragraph -->
+
+
+        <!-- wp:buttons {"layout":{"type":"flex","verticalAlignment":"top"}} -->
+        <div class="wp-block-buttons"><!-- wp:button {"textColor":"luminous-vivid-orange","align":"center","style":{"border":{"radius":"100px"}},"className":"is-style-outline"} -->
+        <div class="wp-block-button aligncenter is-style-outline"><a class="wp-block-button__link has-luminous-vivid-orange-color has-text-color" href="https://www.easyenglishwithcristina.com/product/whatsapp-speaking-club-subscription/?attribute_billing-period=3+Months" style="border-radius:100px"><strong>Add me to the group!</strong></a></div>
+        <!-- /wp:button --></div>
+        <!-- /wp:buttons --></div>
+        <!-- /wp:group --></div>
+        <!-- /wp:column -->
+
+        <!-- wp:column {"verticalAlignment":"center"} -->
+        <div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"backgroundColor":"white","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+        <div class="wp-block-group has-white-background-color has-background"><!-- wp:heading {"textAlign":"center","level":4} -->
+        <h4 class="has-text-align-center" id="h-6-months-subscription">6 months subscription</h4>
+        <!-- /wp:heading -->
+
+		<!-- wp:paragraph {"align":"center",{"style":{"typography":{"textTransform":"uppercase"}}}} -->
+		<p class="has-text-align-center">Get one month for <strong style="text-transform:uppercase">free</strong>!</p>
+		<!-- /wp:paragraph -->
+
+        <!-- wp:paragraph {"align":"center","fontSize":"large"} -->
+        <p class="has-text-align-center has-large-font-size"><strong><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-luminous-vivid-orange-color">99 €</mark></strong><sub>/month</sub></p>
+        <!-- /wp:paragraph -->
+
+
+        <!-- wp:buttons -->
+        <div class="wp-block-buttons"><!-- wp:button {"textColor":"luminous-vivid-orange","align":"center","style":{"border":{"radius":"100px"}},"className":"is-style-outline"} -->
+        <div class="wp-block-button aligncenter is-style-outline"><a class="wp-block-button__link has-luminous-vivid-orange-color has-text-color" href="https://www.easyenglishwithcristina.com/product/whatsapp-speaking-club-subscription/?attribute_billing-period=6+Months" style="border-radius:100px"><strong>Join now</strong></a></div>
+        <!-- /wp:button --></div>
+        <!-- /wp:buttons --></div>
+        <!-- /wp:group --></div>
+        <!-- /wp:column --></div>
+        <!-- /wp:columns -->
+
+        <!-- wp:spacer {"height":"50px"} -->
+        <div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
+        <!-- /wp:spacer -->
+
+        <!-- wp:heading {"textAlign":"center","level":3,"textColor":"white"} -->
+        <h3 class="has-text-align-center has-white-color has-text-color" id="h-are-you-still-in-two-minds">Are you still in two minds<strong>?</strong>🤔</h3>
+        <!-- /wp:heading -->
+
+        <!-- wp:jetpack/send-a-message -->
+        <div class="wp-block-jetpack-send-a-message"><!-- wp:jetpack/whatsapp-button {"countryCode":"39IT","phoneNumber":"3292349751","align":"center"} -->
+        <div class="wp-block-jetpack-whatsapp-button aligncenter is-color-dark"><a class="whatsapp-block__button" href="https://api.whatsapp.com/send?phone=393292349751&amp;text=Hi%2C%20I%20got%20your%20WhatsApp%20information%20from%20your%20website." style="background-color:#25D366;color:#fff" target="_self" rel="noopener noreferrer">Ask more info <br/>directly on WhatsApp!</a></div>
+        <!-- /wp:jetpack/whatsapp-button --></div>
+        <!-- /wp:jetpack/send-a-message -->
+
+       
+
+   
+   
+        <!-- wp:cover {"url":"http://www.easyenglishwithcristina.com/wp-content/uploads/2021/12/pexels-torsten-dettlaff-193003-scaled.jpg","id":801,"dimRatio":0,"focalPoint":{"x":"0.50","y":"0.50"},"minHeight":100,"minHeightUnit":"vh","contentPosition":"bottom center","isDark":false,"align":"full","style":{"color":{"duotone":["#3626a7","#fae8e8"]},"spacing":{"padding":{"bottom":"80px"}}}} -->
+        <div class="wp-block-cover alignfull is-light has-custom-content-position is-position-bottom-center" style="padding-bottom:80px;min-height:100vh" id="course-write-english-business-email"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-801" alt="" src="http://www.easyenglishwithcristina.com/wp-content/uploads/2021/12/pexels-torsten-dettlaff-193003-scaled.jpg" style="object-position:50% 50%" data-object-fit="cover" data-object-position="50% 50%"/><div class="wp-block-cover__inner-container"><!-- wp:columns {"verticalAlignment":"bottom"} -->
+        <div class="wp-block-columns are-vertically-aligned-bottom"><!-- wp:column {"verticalAlignment":"bottom"} -->
+        <div class="wp-block-column is-vertically-aligned-bottom"></div>
+        <!-- /wp:column -->
+
+        <!-- wp:column {"verticalAlignment":"bottom"} -->
+        <div class="wp-block-column is-vertically-aligned-bottom"><!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"300"}},"textColor":"white"} -->
+        <h2 class="has-white-color has-text-color" style="font-style:normal;font-weight:300">Do you want to <br><mark style="background-color:var(--wp--preset--color--secondary)" class="has-inline-color has-white-color"><strong>write Business Emails</strong></mark><br>in <strong>English<mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-tertiary-color">?</mark></strong></h2>
+        <!-- /wp:heading -->
+
+        <!-- wp:paragraph {"textColor":"white"} -->
+        <p class="has-white-color has-text-color">Do you need to write <strong>English emails</strong> at work? Learn and improve your <strong>writing skill</strong>. Learn to present your ideas and <strong>succeed in negotiation</strong>.</p>
+        <!-- /wp:paragraph -->
+
+        
+
+        <!-- wp:cover {"url":"http://www.easyenglishwithcristina.com/wp-content/uploads/2021/03/pexels-polina-tankilevitch-6929185-scaled.jpg","id":950,"dimRatio":0,"focalPoint":{"x":"0.00","y":"0.00"},"minHeight":100,"minHeightUnit":"vh","isDark":false,"align":"full","style":{"color":{"duotone":["#3626a7","#fae8e8"]}}} -->
+        <div class="wp-block-cover alignfull is-light" style="min-height:100vh" id="fluency-practice-speaking-building-confidence"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-950" alt="" src="http://www.easyenglishwithcristina.com/wp-content/uploads/2021/03/pexels-polina-tankilevitch-6929185-scaled.jpg" style="object-position:0% 0%" data-object-fit="cover" data-object-position="0% 0%"/><div class="wp-block-cover__inner-container"><!-- wp:columns -->
+        <div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center"} -->
+        <div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"style":{"border":{"width":"1px","radius":"8px"},"spacing":{"padding":{"right":"6px","left":"6px"}}},"borderColor":"luminous-vivid-orange","className":"is-style-inline","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+        <div class="wp-block-group is-style-inline has-border-color has-luminous-vivid-orange-border-color" style="border-radius:8px;border-width:1px;padding-right:6px;padding-left:6px"><!-- wp:paragraph {"textColor":"luminous-vivid-orange"} -->
+        <p class="has-luminous-vivid-orange-color has-text-color"><strong>Special Offer</strong></p>
+        <!-- /wp:paragraph --></div>
+        <!-- /wp:group -->
+
+        
+     
+
+
+
+    <!-- wp:cover {"url":"https://www.easyenglishwithcristina.com/wp-content/uploads/2022/08/christina-fara-1.png","id":1282,"dimRatio":0,"minHeight":100,"minHeightUnit":"vh","align":"full","style":{"color":{"duotone":["#3626a7","#fae8e8"]}}} -->
+<div class="wp-block-cover alignfull" style="min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-1282" alt="" src="https://www.easyenglishwithcristina.com/wp-content/uploads/2022/08/christina-fara-1.png" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:columns -->
+        <div class="wp-block-columns"><!-- wp:column -->
+        <div class="wp-block-column"></div>
+        <!-- /wp:column -->
+
+        <!-- wp:column {"verticalAlignment":"center"} -->
+        <div class="wp-block-column is-vertically-aligned-center">
+			<!-- wp:paragraph {"textColor":"tertiary"} -->
+			<p class="has-tertiary-color has-text-color"><a href="https://www.easyenglishwithcristina.com/about-me/" data-type="URL" data-id="dfdfd">Get to know the teacher</a></p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:heading {"style":{"typography":{"fontWeight":"300"}}} -->
+        <h2 style="font-weight:300">Can't find <br>an <strong>offer that suits your needs</strong>!</h2>
+        <!-- /wp:heading -->
+
+        <!-- wp:paragraph -->
+        <p>Leave your name and we will get back to you</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:jetpack/contact-form {"subject":"[Easy English with Cristina] Nice to meet you","to":"info@easyenglishwithcristina.com"} -->
+<div class="wp-block-jetpack-contact-form"><!-- wp:jetpack/field-name {"required":true,"requiredText":"(required)","width":50} /-->
+
+	<!-- wp:jetpack/field-email {"required":true,"requiredText":"(required)","width":50} /-->
+
+	<!-- wp:jetpack/field-textarea {"label":"Message","requiredText":"(required)"} /-->
+
+	<!-- wp:jetpack/button {"element":"button","text":"Contact Me","lock":{"remove":true}} /--></div>
+	<!-- /wp:jetpack/contact-form --></div>
+        <!-- /wp:column --></div>
+        <!-- /wp:columns --></div></div>
+        <!-- /wp:cover -->
