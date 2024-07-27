e782900a5b9c46baebe93a0ee2f2f2d2af509b34
Giacomo Secchi <giacomo.secchi@gmail.com>
2024-07-27 18:35:23 +0200

    refactor: Remove deprecated code and update template references

---
+++ patterns/main-navigation.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Title: Main navigation
+ * Slug: write-white/main-navigation
+ */
+?>
+
+<!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex"},"style":{"typography":{"fontWeight":"400","textTransform":"uppercase"}},"ariaLabel":"<?php esc_attr_e( 'About', 'easyenglishwithcristinatheme' ); ?>"} -->
+
+    <!-- wp:navigation-link {"label":"<?php esc_html_e( 'English for Beginners', 'easyenglishwithcristinatheme' ); ?>","url":"#"} /-->
+    <!-- wp:navigation-link {"label":"<?php esc_html_e( 'English for Students', 'easyenglishwithcristinatheme' ); ?>","url":"#"} /-->
+    <!-- wp:navigation-link {"label":"<?php esc_html_e( 'English for Business', 'easyenglishwithcristinatheme' ); ?>","url":"#"} /-->
+
+<!-- /wp:navigation -->
