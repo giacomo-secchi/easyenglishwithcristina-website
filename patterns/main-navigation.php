f0b72cbf14ce508c99fa1692cba89202df489497
Giacomo Secchi <giacomo.secchi@gmail.com>
2024-07-28 17:05:34 +0200

    refactor: Update main navigation layout and links

--- patterns/main-navigation.php
+++ patterns/main-navigation.php
@@ -5,10 +5,10 @@
  */
 ?>
 
-<!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex"},"style":{"typography":{"fontWeight":"400","textTransform":"uppercase"}},"ariaLabel":"<?php esc_attr_e( 'About', 'easyenglishwithcristinatheme' ); ?>"} -->
-
-    <!-- wp:navigation-link {"label":"<?php esc_html_e( 'English for Beginners', 'easyenglishwithcristinatheme' ); ?>","url":"#"} /-->
-    <!-- wp:navigation-link {"label":"<?php esc_html_e( 'English for Students', 'easyenglishwithcristinatheme' ); ?>","url":"#"} /-->
-    <!-- wp:navigation-link {"label":"<?php esc_html_e( 'English for Business', 'easyenglishwithcristinatheme' ); ?>","url":"#"} /-->
+<!-- wp:navigation {"style":{"typography":{"fontWeight":"400","textTransform":"uppercase"},"layout":{"type":"flex","selfStretch":"fill"}},"layout":{"type":"flex","justifyContent":"center"},"ariaLabel":"<?php esc_attr_e( 'About', 'easyenglishwithcristinatheme' ); ?>"} -->
+    
+    <!-- wp:navigation-link {"label":"<?php esc_html_e( 'English for Beginners', 'easyenglishwithcristinatheme' ); ?>","url":"#english-for-beginners"} /-->
+    <!-- wp:navigation-link {"label":"<?php esc_html_e( 'English for Students', 'easyenglishwithcristinatheme' ); ?>","url":"#english-for-students"} /-->
+    <!-- wp:navigation-link {"label":"<?php esc_html_e( 'English for Business', 'easyenglishwithcristinatheme' ); ?>","url":"#english-for-business"} /-->
 
 <!-- /wp:navigation -->
