de4e0a8eb656725536445f5af99649ea19392821
Giacomo Secchi <giacomo.secchi@gmail.com>
2024-08-10 16:16:42 +0200

    refactor: Update site title styles and remove deprecated code

---
+++ patterns/branding.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Title: Site title with tagline
+ * Slug: write-white/branding
+ * Categories: header
+ * Keywords: Basic Header, header, brand, site title, tagline
+ * Viewport Width: 300
+ * Block Types: core/template-part/header, core/template-part/footer
+ * Inserter: true
+ */
+?>
+
+<!-- wp:group {"style":{"layout":{"selfStretch":"fixed","flexSize":"180px"}}} -->
+<div class="wp-block-group">
+    <!-- wp:site-title /-->
+</div>
+<!-- /wp:group -->
+
--- patterns/main-navigation.php
+++ patterns/main-navigation.php
@@ -4,8 +4,7 @@
  * Slug: write-white/main-navigation
  */
 ?>
-
-<!-- wp:navigation {"style":{"typography":{"fontWeight":"400","textTransform":"uppercase"},"layout":{"type":"flex","selfStretch":"fill"}},"layout":{"type":"flex","justifyContent":"center"},"ariaLabel":"<?php esc_attr_e( 'About', 'easyenglishwithcristinatheme' ); ?>"} -->
+<!-- wp:navigation {"style":{"typography":{"fontWeight":"300","textTransform":"uppercase"},"layout":{"type":"flex","selfStretch":"fill"}},"layout":{"type":"flex","justifyContent":"center"},"ariaLabel":"<?php esc_attr_e( 'About', 'easyenglishwithcristinatheme' ); ?>"} -->
     
     <!-- wp:navigation-link {"label":"<?php esc_html_e( 'English for Beginners', 'easyenglishwithcristinatheme' ); ?>","url":"#english-for-beginners"} /-->
     <!-- wp:navigation-link {"label":"<?php esc_html_e( 'English for Students', 'easyenglishwithcristinatheme' ); ?>","url":"#english-for-students"} /-->
