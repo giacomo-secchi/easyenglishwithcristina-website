479368944036392573ad96f1dc522de0b8d14d3a
Giacomo Secchi <giacomosecchi@gmail.com>
2025-03-16 17:21:56 +0100

    Replace teacher image and update banner layout; add 'About Me' link to footer

--- patterns/banner-teacher.php
+++ patterns/banner-teacher.php
@@ -10,76 +10,73 @@
  */
 ?>
 
-<!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/pexels-antonio-quagliata-227433.jpg","id":323,"dimRatio":50,"overlayColor":"primary","isUserOverlayColor":true,"minHeight":100,"minHeightUnit":"vh","align":"full","style":{"color":{"duotone":"var:preset|duotone|blue-and-lavender"}}} -->
-<div class="wp-block-cover alignfull" style="min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim"></span><img class="wp-block-cover__image-background wp-image-323" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/pexels-antonio-quagliata-227433.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container">
+
+
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|90","bottom":"var:preset|spacing|90"}}},"backgroundColor":"base","layout":{"type":"constrained","justifyContent":"right"}} -->
+<div class="wp-block-group alignfull has-base-background-color has-background" style="padding-top:var(--wp--preset--spacing--90);padding-bottom:var(--wp--preset--spacing--90)"><!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide">
+	
+<!-- wp:column {"width":"40%"} -->
+<div class="wp-block-column" style="flex-basis:40%"><!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase"},"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}},"textColor":"primary","fontSize":"small"} -->
+<p class="has-primary-color has-text-color has-link-color has-small-font-size" style="text-transform:uppercase"><?php esc_html_e( 'About the teacher', 'easyenglishwithcristina' ); ?></p>
+<!-- /wp:paragraph -->
 
  
-	<!-- wp:columns {"align":"wide","style":{"layout":{"selfStretch":"fill","flexSize":null}}} -->
-    <div class="wp-block-columns alignwide">
-			<!-- wp:column {"width":"38%"} -->
-			<div class="wp-block-column" style="flex-basis:38%"></div>
-			<!-- /wp:column -->
-			 
-			<!-- wp:column {"verticalAlignment":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}},"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60","right":"var:preset|spacing|60"}}},"backgroundColor":"white-semi-trasparent","textColor":"contrast"} -->
-			<div class="wp-block-column is-vertically-aligned-center has-contrast-color has-white-semi-trasparent-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)">
-				<!-- wp:paragraph {"align":"left","style":{"typography":{"textTransform":"uppercase","fontWeight":"300"},"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}}},"textColor":"secondary"} -->
-				<p class="has-text-align-left has-secondary-color has-text-color has-link-color" style="text-transform:uppercase;font-weight:300"><?php esc_html_e( 'About the Teacher', 'easyenglishwithcristina' ); ?></p>
+<!-- wp:heading {"style":{"spacing":{"margin":{"top":"0"}}}} -->
+<h2 class="wp-block-heading" style="margin-top:0"><?php echo wp_kses_post( __('Hello and Welcome!<br> Nice to meet you.', 'easyenglishwithcristina' ) ); ?></h2>
+<!-- /wp:heading -->
+				 
+				
+				<!-- wp:paragraph {"fontSize":"display"} -->
+				<p class="has-display-font-size"><?php echo wp_kses_post( 'I’m Cristina and I am a <strong>TESOL certified English teacher</strong> with <strong>+30 years of experience.</strong>', 'easyenglishwithcristina' ); ?></p>
+				<!-- /wp:paragraph -->
+				 		 
+				<!-- wp:paragraph -->
+				<p><?php esc_html_e( 'My personal life is full of passion for languages, travelling, and socialization. That’s why I enjoy working with people and being a teacher is not what I do any more, it has become who I am.', 'easyenglishwithcristina' ); ?></p>
 				<!-- /wp:paragraph -->
 
-				<!-- wp:heading {"style":{"spacing":{"margin":{"top":"0"}}}} -->
-				<h2 class="wp-block-heading" id="lorem-ipsum" style="margin-top:0"><?php echo wp_kses_post( __('Hello and Welcome!<br> Nice to meet you.', 'easyenglishwithcristina' ) ); ?></h2>
-				<!-- /wp:heading -->
-                 
-                <!-- wp:paragraph {"placeholder":"Content…","fontSize":"display"} -->
-                <p class="has-display-font-size"><?php echo wp_kses_post( 'I’m Cristina and I am a <strong>TESOL certified English teacher</strong> with <strong>+10 years of experience.</strong>', 'easyenglishwithcristina' ); ?></p>
-                <!-- /wp:paragraph -->
-				
-                <!-- wp:columns -->
-				<div class="wp-block-columns"><!-- wp:column -->
-					<div class="wp-block-column">
-						<!-- wp:paragraph -->
-						<p><?php echo wp_kses_post( 'I can provide you with techniques that can <strong>improve your English within a few months</strong>.', 'easyenglishwithcristina' ); ?></p>
-						<!-- /wp:paragraph -->
-						<!-- wp:image {"id":371,"sizeSlug":"large","linkDestination":"none","className":"is-style-rounded","style":{"border":{"radius":"30px"}}} -->
-						<figure class="wp-block-image size-large has-custom-border is-style-rounded"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/WhatsApp-Image-2021-05-28-at-17.03.30.jpeg" alt="" class="wp-image-371" style="border-radius:30px"/></figure>
-						<!-- /wp:image -->
-					</div>
-					<!-- /wp:column -->
-					<!-- wp:column -->
-					<div class="wp-block-column">
-							<!-- wp:paragraph -->
-							<p><?php echo esc_html_e( 'I have been teaching since college where I had the opportunity to work part time as a teacher. In no time, being a teacher became more than just a career for me…', 'easyenglishwithcristina' ); ?></p>
-							<!-- /wp:paragraph -->
-			 
-							<!-- wp:paragraph -->
-							<p><?php esc_html_e( 'I was eager to learn, so I participated in some teaching training courses including TESOL, training kids, teenagers, adults, and IELTS preparation courses.', 'easyenglishwithcristina' ); ?></p>
-							<!-- /wp:paragraph -->
+				<!-- wp:paragraph -->
+				<p><?php echo wp_kses_post( 'I possess soft skills like:', 'easyenglishwithcristina' ); ?></p>
+				<!-- /wp:paragraph --> 	
+				 
+				<!-- wp:list -->
+				<ul class="wp-block-list"><!-- wp:list-item -->
+				<li><?php esc_html_e( 'facilitating learning process using different digital software', 'easyenglishwithcristina' ); ?></li>
+				<!-- /wp:list-item -->
 
-						</div>
-						<!-- /wp:column -->
-					<!-- wp:column -->
-					<div class="wp-block-column">             
-						<!-- wp:paragraph -->
-						<p><?php esc_html_e( 'My personal life is full of passion for languages, travelling, and socialization. That’s why I enjoy working with people and being a teacher is not what I do any more, it has become who I am.', 'easyenglishwithcristina' ); ?></p>
-						<!-- /wp:paragraph -->
+				<!-- wp:list-item -->
+				<li><?php esc_html_e( 'presenting webinars', 'easyenglishwithcristina' ); ?></li>
+				<!-- /wp:list-item -->
 
-						<!-- wp:paragraph -->
-						<p><?php echo wp_kses_post( 'I possess soft skills like: <strong>facilitating learning process using different digital software, presenting webinars, and preparing goal oriented lesson plans.</strong>', 'easyenglishwithcristina' ); ?></p>
-						<!-- /wp:paragraph --> 
-					</div>
-					<!-- /wp:column -->
+				<!-- wp:list-item -->
+				<li><?php esc_html_e( 'preparing goal oriented lesson plans', 'easyenglishwithcristina' ); ?></li>
+				<!-- /wp:list-item --></ul>
+				<!-- /wp:list -->
+
+				<!-- wp:buttons -->
+				<div class="wp-block-buttons">
+				<!-- wp:button {"backgroundColor":"secondary"} -->
+				<div class="wp-block-button"><a class="wp-block-button__link has-secondary-background-color has-background wp-element-button" href="<?php echo esc_url( home_url( 'about-me/' ) ); ?>"><?php esc_html_e( 'Learn More About Me →', 'easyenglishwithcristina' ); ?></a></div>
+					<!-- /wp:button -->
 				</div>
-				<!-- /wp:columns -->
+				<!-- /wp:buttons -->
+ 
 
-			</div>
-			<!-- /wp:column -->
+</div>
+<!-- /wp:column -->
+<!-- wp:column {"width":"60%"} -->
+<div class="wp-block-column" style="flex-basis:60%">
+ 
+ 
+<!-- wp:image {"linkDestination":"none","className":"is-style-rounded","style":{"border":{"radius":"30px"}}} -->
+<figure class="wp-block-image has-custom-border is-style-rounded"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/cristina-fara.jpg" alt="" style="border-radius:30px;"/></figure>
+<!-- /wp:image -->
+ 
 
+</div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
 
-		</div>
-		<!-- /wp:columns -->
-
-  
-
-</div></div>
-<!-- /wp:cover -->
  
