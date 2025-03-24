8f199e8efd548d35ad1372ec03aa29dc27c663c0
Alberto-Giusti <spaghetti_boys@hotmail.it>
2025-03-24 18:05:08 +0100

    Add newsletter subscription banner pattern and styles; include new image

---
+++ patterns/banner-subscribe-newsletter.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Title: Banner subscribe newsletter
+ * Slug: easyenglishwithcristina/banner-subscribe-newsletter
+ * Categories: banner, call-to-action
+ * Keywords: banner
+ * Viewport Width: 1500
+ * Inserter: true
+ */
+?>
+
+<!-- wp:group {"style":{"background":{"backgroundImage":{"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/banner-subscribe-newsletter.png","source":"file","title":"speaking-club"},"backgroundPosition":"100% 100%"},"elements":{"link":{"color":{"text":"var:preset|color|white-semi-trasparent"}}},"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"}}},"backgroundColor":"primary","textColor":"white","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-white-color has-primary-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)">
+<!-- wp:mailchimp/mailchimp /--></div>
+<!-- /wp:group -->
