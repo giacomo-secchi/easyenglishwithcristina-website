6922f975d0ee3e4c293b7f520ce394d87cc11a49
Alberto-Giusti <spaghetti_boys@hotmail.it>
2025-04-10 17:35:15 +0200

    Add newsletter subscription banner styles and update background image settings

--- patterns/banner-subscribe-newsletter.php
+++ patterns/banner-subscribe-newsletter.php
@@ -9,7 +9,7 @@
  */
 ?>
 
-<!-- wp:group {"style":{"background":{"backgroundImage":{"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/banner-subscribe-newsletter.png","source":"file","title":"speaking-club"},"backgroundPosition":"100% 100%"},"elements":{"link":{"color":{"text":"var:preset|color|white-semi-trasparent"}}},"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"}}},"backgroundColor":"primary","textColor":"white","layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-white-color has-primary-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)">
+<!-- wp:group {"style":{"background":{"backgroundImage":{"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/banner-subscribe-newsletter.png","source":"file","title":"speaking-club"},"backgroundPosition":"100% 100%","backgroundRepeat":"no-repeat"},"elements":{"link":{"color":{"text":"var:preset|color|white-semi-trasparent"}}},"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"}}},"backgroundColor":"primary","textColor":"white","className":"banner-subscribe-newsletter","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-white-color has-primary-background-color has-text-color has-background has-link-color banner-subscribe-newsletter" style="padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)">
 <!-- wp:mailchimp/mailchimp /--></div>
 <!-- /wp:group -->
