976ffcebf3c9c3fb680e2f1583894803a9a3feb2
Giacomo Secchi <giacomosecchi@gmail.com>
2025-09-17 15:21:13 +0200

    Remove Mailchimp and add Brevo to Newsletter banner

--- themes/easyenglishwithcristina-theme/patterns/banner-subscribe-newsletter.php
+++ themes/easyenglishwithcristina-theme/patterns/banner-subscribe-newsletter.php
@@ -9,7 +9,32 @@
  */
 ?>
 
-<!-- wp:group {"style":{"background":{"backgroundImage":{"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/banner-subscribe-newsletter.png","source":"file","title":"speaking-club"},"backgroundPosition":"100% 100%","backgroundRepeat":"no-repeat"},"elements":{"link":{"color":{"text":"var:preset|color|white-semi-trasparent"}}},"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"}}},"backgroundColor":"primary","textColor":"white","className":"banner-subscribe-newsletter","layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-white-color has-primary-background-color has-text-color has-background has-link-color banner-subscribe-newsletter" style="padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)">
-<!-- wp:mailchimp/mailchimp /--></div>
+
+
+<!-- wp:group {"align":"full","className":"banner-subscribe-newsletter","style":{"background":{"backgroundImage":{"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/banner-subscribe-newsletter.png","source":"file","title":"pexels-quill-j-154796843-33425453"},"backgroundSize":"contain","backgroundAttachment":"scroll","backgroundRepeat":"no-repeat","backgroundPosition":"50% 100%"},"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70"}}},"backgroundColor":"primary","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull banner-subscribe-newsletter has-primary-background-color has-background" style="padding-top:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70)"><!-- wp:heading {"textAlign":"left","level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<h3 class="wp-block-heading has-text-align-left has-white-color has-text-color has-link-color">Want to level up your English? ✨</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<p class="has-white-color has-text-color has-link-color">Join the <em>American English</em> Blog newsletter! Get the latest posts (like this one), expert learning tips, and free resources—delivered straight to your inbox. <strong>Sign up now</strong>!<br></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list {"className":"is-style-none"} -->
+<ul class="wp-block-list is-style-none"><!-- wp:list-item {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<li class="has-white-color has-text-color has-link-color">✔️ Practical tips to boost your skills,</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<li class="has-white-color has-text-color has-link-color">✔️ Exclusive learning strategies,</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<li class="has-white-color has-text-color has-link-color">✔️ Free resources (worksheets, PDFs, and more!).<br><br></li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
+
+<!-- wp:shortcode -->
+[sibwp_form id=3]
+<!-- /wp:shortcode --></div>
 <!-- /wp:group -->
